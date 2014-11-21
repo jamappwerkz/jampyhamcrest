4f0686a23edbdf9bd52c382822408ec7262c00b5
Tomasz Sokolowski <TSokolowski@advaoptical.com>
2014-11-21 03:33:29 +0100

    app prepd for distribution;

---
+++ setup.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+from setuptools import setup
+
+setup(name='jampyhamcrest',
+      version='0.1',
+      description='JamApp PyHamcrest matchers.',
+      url='https://github.com/jamappwerkz/jampyhamcrest',
+      author='JamApp Werkz',
+      author_email='jamappwerkz@gmail.com',
+      license='GPLv2',
+      packages=['jampyhamcrest'],
+      zip_safe=False)
